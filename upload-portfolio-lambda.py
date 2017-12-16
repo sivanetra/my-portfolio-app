7c25cc3c6b146157766d86c20c774f60f39113ec
Sivanetra <netra.nadimpalli@gmail.com>
2017-12-16 10:46:58 -0600

    feature: updates to deploy lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -9,12 +9,25 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:917817934803:deployPortfolioTopic')
     try:
+        job = event.get("CodePipeline.job")
+        #TO RUN OUTSIDE OF CODEPIPELINE
+        location = {
+            "bucketName": 'netra-portfolio-app-build',
+            "objectKey": 'netra-portfolio-app.zip'
+        }
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print("Building portfolio from " + str(location))
+
         s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
         portfolio_bucket = s3.Bucket('netra-portfolio-app')
-        portfolio_build_bucket = s3.Bucket('netra-portfolio-app-build')
+        portfolio_build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = io.BytesIO()
-        portfolio_build_bucket.download_fileobj('netra-portfolio-app.zip', portfolio_zip)
+        portfolio_build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -25,6 +38,9 @@ def lambda_handler(event, context):
         portfolio_zip.close()
         print('Job done')
         topic.publish(Subject="portfolio Deployed", Message="Your deploy is successful")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="portfolio deploy failed", Message="Your deploy is failed")
         raise
